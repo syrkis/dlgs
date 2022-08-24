1007fc88aa878c41b16e214804728aabd6c05d46
Noah Syrkis <noah@syrkis.com>
2022-08-24 17:47:30 +0200

    lab 0. TODO: add detect winner

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+#   deep learning in simulations and games
+# by: Noah Syrkis
+
+# imports
+from src import *
+
+
+# main
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
